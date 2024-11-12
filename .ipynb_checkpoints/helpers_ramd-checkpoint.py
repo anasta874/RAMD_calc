86c15ca1d1f63f6b9e6fdc48e649954afdf17198
Anastasia Poluzerova <anasta874@gmail.com>
2024-11-12 12:19:41 +0100

    Update

--- .ipynb_checkpoints/helpers_ramd-checkpoint.py
+++ .ipynb_checkpoints/helpers_ramd-checkpoint.py
@@ -3,26 +3,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import ScalarFormatter, FuncFormatter
 
-# Загрузка данных из Excel
+# Load data from Excel
 def load_excel(file_path, skip_rows=2):
     data = pd.read_excel(file_path, header=None).to_numpy()
     return data[skip_rows:, :]
 
-# Фильтрация данных
-def filter_data(time, values, n, N_cut, tau_frac):
-    t_fit, n_fit = [], []
-    for i in range(len(time)):
-        if values[i] > N_cut and time[i] > tau_frac:
-            t_fit.append(time[i])
-            n_fit.append(n[i]) 
-    return t_fit, n_fit
+# Function to calculate tau and lambda values and populate lists
+def calculate_dissociation_rates(t, n, N, N_cut_max, tau_frac, tau_var, lambda_var, pend_var):
+    for N_cut in range(N_cut_max):
+        # Filter data based on N_cut and tau_frac
+        t_fit = t[(N > N_cut) & (t > tau_frac)]
+        n_fit = n[(N > N_cut) & (t > tau_frac)]
 
-# Построение графика с выделением точки обрезки
+        if len(t_fit) > 1:  # Check if there is enough data for fitting
+            # Linear fit
+            pend = np.polyfit(t_fit, n_fit, 1)
+            tau = round((1 / abs(pend[0])) / 1e4, 15)  # Scale tau
+            lambda_ = round(pend[0] * 1e4, 15)         # Scale lambda
+
+            # Save results in the provided lists
+            tau_var.append(tau)
+            lambda_var.append(lambda_)
+            pend_var.append(pend)
+
+# Plot tau with a highlight on the cutoff point
 def plot_tau_with_cutoff(x_values, y_values, N_cut, tau_var, force_value, output_file=None):
     plt.figure()
     plt.plot(x_values, y_values, label=r'$\tau$ values')
     vline = plt.axvline(x=N_cut, color='red', linestyle='--', linewidth=2)
-    plt.legend([vline], ['Cropping point'], loc='best')
+    plt.legend([vline], ['Cutoff point'], loc='best')
     plt.title(f"Force {force_value}", fontsize=18)
     plt.ylabel(r'$\tau$, ps', fontsize=18)
     plt.xlabel(r'$N_{cut}$', fontsize=18)
@@ -35,9 +44,9 @@ def plot_tau_with_cutoff(x_values, y_values, N_cut, tau_var, force_value, output
         plt.savefig(output_file)
     plt.show()
 
-# Построение начального линейного участка в логарифмических координатах
+# Plot the initial linear region in logarithmic coordinates
 def plot_init_part_ln_ln(force_value, n, t, t0, tend, output_file=None):
-    # Фильтрация положительных значений в диапазоне t0:tend
+    # Filter positive values in the range t0:tend
     valid_indices = (t[t0:tend] > 0) & (n[t0:tend] < 0)
     filtered_t = t[t0:tend][valid_indices]
     filtered_n = n[t0:tend][valid_indices]
@@ -57,16 +66,15 @@ def plot_init_part_ln_ln(force_value, n, t, t0, tend, output_file=None):
     # plt.show()
     return p, pinit
 
-
-# Функция для построения графика ln(N(t)/N(0))
+# Function to plot ln(N(t)/N(0))
 def plot_lnN(force_value, t, t_fit, n, tau_frac, pend, pinit, output_file=None):
     plt.figure()
     plt.plot(t, n, 'o', color='magenta', linewidth=1.5, label='Data')
     plt.plot(t_fit, np.polyval(pend, t_fit), color='blue', linewidth=1.5, label='Fitted Line')
-    # Фильтрация положительных значений для логарифма
+    # Filter positive values for the logarithm
     valid_indices = (t > 0) & (t < tau_frac)
     valid_t = t[valid_indices]
-    # Построение графика экспоненциального участка с фильтрацией
+    # Plot the exponential region with filtering
     plt.plot(valid_t, -np.exp(np.polyval(pinit, np.log(valid_t))), '--', color='black', linewidth=1.5, label='Exponential Fit')
     plt.axvline(x=tau_frac, linestyle='--', color='red')
     plt.ylim([1.1 * np.min(n), 0.1])
@@ -81,8 +89,7 @@ def plot_lnN(force_value, t, t_fit, n, tau_frac, pend, pinit, output_file=None):
         plt.savefig(output_file)
     plt.show()
 
-
-# Функция для построения графика N(t)
+# Function to plot N(t)
 def plot_N(force_value, t, t_fit, N, N0, tau_frac, pend, pinit, output_file=None):
     t_fit = np.array(t_fit)
     plt.figure()
@@ -100,36 +107,4 @@ def plot_N(force_value, t, t_fit, N, N0, tau_frac, pend, pinit, output_file=None
     plt.legend()
     if output_file:
         plt.savefig(output_file)
-    plt.show()
-
-#  Построение двух графиков в одном:
-def two_in_one(force_value, t, t_fit, n, N, N0, tau_frac, pend, pinit, xlim1, ylim1, xlim2, axs, output_file='output_two_in_one.png'):
-    pend, pinit, t_fit = np.array(pend), np.array(pinit), np.array(t_fit)
-    fig, ax1 = plt.subplots(figsize=(7, 5)) 
-    plt.subplots_adjust(left=0.15, right=0.9, top=0.85, bottom=0.15) 
-    ax1.plot(t, n, 'o', color='magenta', markersize=3, linewidth=1.5)
-    if len(t[t < tau_frac]) > 0:
-        ax1.plot(t_fit, np.polyval(pend, t_fit), color='blue', linewidth=1)
-        ax1.plot(t[t < tau_frac], -np.exp(np.polyval(pinit, np.log(t[t < tau_frac]))), '--', color='black', linewidth=1)
-    ax1.set(xlim=xlim1, ylim=ylim1, xlabel=r'$t, \mathrm{ps}$', ylabel=r'$\ln(N(t)/N(0))$')
-    ax1.tick_params(axis='both', labelsize=10)
-    ax1.xaxis.set_major_formatter(FuncFormatter(lambda x, _: f'{x*1e-5:.1f}'))  # Оси X без тысяч
-    ax1.grid(False)
-    ax1.spines['top'].set_visible(False)
-    ax1.spines['right'].set_visible(False)
-
-    # Построение второго графика: N(t) vs t (в маленьком окошке)
-    ax2 = fig.add_axes([0.55, 0.45, 0.4, 0.4])
-    ax2.plot(t, N, 'o', color='magenta', markersize=3, linewidth=1.5)
-    if len(t[t < tau_frac]) > 0:
-        ax2.plot(t[t < tau_frac], N[0] * np.exp(-np.exp(pinit[1]) * t[t < tau_frac] ** pinit[0]), '--', color='black', linewidth=1)
-    ax2.plot(t_fit, N0 * np.exp(pend[1]) * np.exp(pend[0] * t_fit), color='blue', linewidth=1)
-    ax2.axvline(x=tau_frac, linestyle='--', color='red', linewidth=1)
-    ax2.set(xlim=xlim2, ylim=[0, N0 * 1.2], xlabel=r'$t, \mathrm{ps}$', ylabel=r'$N(t)$')
-    ax2.tick_params(axis='both', labelsize=8)
-    ax2.xaxis.set_major_formatter(FuncFormatter(lambda x, _: f'{x*1e-5:.1f}'))
-    ax2.grid(False)
-    ax2.spines['top'].set_visible(False)
-    ax2.spines['right'].set_visible(False)
-    plt.savefig(output_file, dpi=300, bbox_inches='tight')
     plt.show()
